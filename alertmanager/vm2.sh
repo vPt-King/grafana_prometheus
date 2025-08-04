da3d630835d7a64faa3c9c471842072c5928e7b4
Thanh <78953786+vPt-King@users.noreply.github.com>
2025-08-04 10:56:36 +0700

    Update vm2.sh change ip to pro

--- alertmanager/vm2.sh
+++ alertmanager/vm2.sh
@@ -4,5 +4,5 @@ docker run -d --name alertmanager \
   prom/alertmanager:v0.26.0 \
   --config.file=/etc/alertmanager/alertmanager.yml \
   --cluster.listen-address=0.0.0.0:9094 \
-  --cluster.advertise-address=192.168.1.11:9094 \
-  --cluster.peer=192.168.1.10:9094
+  --cluster.advertise-address=10.0.139.117:9094 \
+  --cluster.peer=10.0.139.116:9094
